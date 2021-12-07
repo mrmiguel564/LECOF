c44c2ff881145fbc1087a5aeedc96b54d8744cc1
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2021-12-07 00:40:08 -0300

    [reporte clima] create

---
+++ app/src/main/java/com/example/quintero/Reporte_Clima.java
@@ -0,0 +1,14 @@
+package com.example.quintero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reporte_Clima extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte_clima);
+    }
+}
