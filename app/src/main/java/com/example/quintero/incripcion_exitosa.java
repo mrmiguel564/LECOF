d8388bdad1a23e05040fac4186e5a7620012dc12
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2021-12-07 20:56:51 -0300

    [volver al menu]

--- app/src/main/java/com/example/quintero/incripcion_exitosa.java
+++ app/src/main/java/com/example/quintero/incripcion_exitosa.java
@@ -2,7 +2,9 @@ package com.example.quintero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class incripcion_exitosa extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class incripcion_exitosa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incripcion_exitosa);
+    }
+
+    public void onClick(View view) {
+        Intent miIntent1 = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(miIntent1);
     }
 }
