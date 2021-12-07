e3f782e0ea1734129d406a5af53e1141e828b6ca
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2021-12-07 01:11:46 -0300

    asd

--- app/src/main/java/com/example/quintero/Reporte_Clima.java
+++ app/src/main/java/com/example/quintero/Reporte_Clima.java
@@ -2,7 +2,9 @@ package com.example.quintero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Reporte_Clima extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Reporte_Clima extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_clima);
+    }
+    public void onClick(View view){
+        Intent miIntent = new Intent(Reporte_Clima.this, IncripcionDeportiva.class);
+        startActivity(miIntent);
+
     }
 }
