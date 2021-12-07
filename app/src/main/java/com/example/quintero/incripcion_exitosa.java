27995e83e76b4510321bbc1cd5f016219510ff06
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2021-12-07 20:46:58 -0300

    [mensaje post incripcion]

---
+++ app/src/main/java/com/example/quintero/incripcion_exitosa.java
@@ -0,0 +1,14 @@
+package com.example.quintero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class incripcion_exitosa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incripcion_exitosa);
+    }
+}
