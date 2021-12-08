992579d960c4d8ee7f06021ab3607d14769c60e3
Galda <felipe.galdames@alumnos.uv.cl>
2021-12-07 23:33:05 -0300

    Añadidos listeners para talleres
    Revisar si es que conectan al formulario

---
+++ app/src/main/java/com/example/quintero/FormularioTalleres.java
@@ -0,0 +1,17 @@
+package com.example.quintero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FormularioTalleres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_talleres);
+    }
+
+}
--- app/src/main/java/com/example/quintero/TalleresDeportivos.java
+++ app/src/main/java/com/example/quintero/TalleresDeportivos.java
@@ -1,17 +1,99 @@
 package com.example.quintero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.quintero.databinding.ActivityMainBinding;
 
 public class TalleresDeportivos extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    CardView cardFutbol;
+    CardView cardVolleyball;
+    CardView cardBasketball;
+    CardView cardBoxeo;
+    CardView cardRugby;
+    CardView cardTenis;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talleres_deportivos);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        cardFutbol = findViewById(R.id.cardFutbol);
+        cardVolleyball = findViewById(R.id.cardVolleyball);
+        cardBasketball = findViewById(R.id.cardBasketball);
+        cardBoxeo = findViewById(R.id.cardBoxeo);
+        cardRugby = findViewById(R.id.cardRugby);
+        cardTenis = findViewById(R.id.cardTenis);
+
+        cardFutbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 1");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+
+        cardVolleyball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 2");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+
+        cardBasketball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 3");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+
+        cardBoxeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 4");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+
+        cardRugby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 5");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+
+        cardTenis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clickeo en Taller 6");
+                Intent miIntent = new Intent(getApplicationContext(), FormularioTalleres.class);
+                startActivity(miIntent);
+            }
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 }
