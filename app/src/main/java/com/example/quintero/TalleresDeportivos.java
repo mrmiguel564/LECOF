2b7d521f7fb3a83d7ddcf746b9724fb803e5f414
Galda <felipe.galdames@alumnos.uv.cl>
2021-12-08 22:22:48 -0300

    Finalizacion de front end formulario

--- app/src/main/java/com/example/quintero/TalleresDeportivos.java
+++ app/src/main/java/com/example/quintero/TalleresDeportivos.java
@@ -27,8 +27,6 @@ public class TalleresDeportivos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talleres_deportivos);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         cardFutbol = findViewById(R.id.cardFutbol);
         cardVolleyball = findViewById(R.id.cardVolleyball);
